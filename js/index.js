c5d84a0652897670c49c9633358ef3d7775d2cca
Audrey Gu <audreymgu@gmail.com>
2019-02-18 10:24:10 -0500

    Fixes and cleanup.

--- js/index.js
+++ js/index.js
@@ -5,19 +5,19 @@ var i;
 
 for (i = 0; i < dropdownBtns.length; i++) {
   dropdownBtns[i].addEventListener("click", function() {
-    if (this.classList.contains("btn-expand--active")) {
-      this.classList.toggle("btn-expand--active");
+    // Change button icon on click
+    if (this.classList.contains("btn--expand--active")) {
+      this.classList.toggle("btn--expand--active");
       this.childNodes[0].classList.remove("fa-chevron-down");
       this.childNodes[0].classList.add("fa-chevron-left");
     } else {
-      this.classList.toggle("btn-expand--active");
+      this.classList.toggle("btn--expand--active");
       this.childNodes[0].classList.remove("fa-chevron-left");
       this.childNodes[0].classList.add("fa-chevron-down");
     }
-
+    // Open or close dropdown panel
     let dropPanel = this.parentNode.parentNode.nextElementSibling;
     dropPanel.classList.toggle("contact-dropdown--active");
-
     if (dropPanel.style.maxHeight) {
       dropPanel.style.maxHeight = null;
     } else {
